9e3cce2e1921380be09f952186fea44197420362
DevendraDK3 <70775513+DevendraDK3@users.noreply.github.com>
2022-02-16 16:43:55 +0530

    quizapp

---
+++ js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+    {
+    numb: 6,
+    question: "What does DBMS stand for?",
+    answer: "Database management system",
+    options: [
+      "Database management system",
+      "Database manager system",
+      "Database management service",
+      "Database manager service"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Information in the computer is called",
+    answer: "Stored data",
+    options: [
+      "Data",
+      "Symbol",
+      "Number",
+      "Stored data"
+    ]
+  },
+  {
+    numb: 8,
+    question: "'OS' computer abbreviation usually means?",
+    answer: "Operating System",
+    options: [
+      "Order of Significance",
+      "Open Software",
+      "Optical Sensor",
+      "Operating System"
+    ]
+  },
+  {
+    numb: 9,
+    question: "…………… is processed in information by a computer processor?",
+    answer: "Data",
+    options: [
+      "Number",
+      "Data",
+      "Processor",
+      "Input"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Meaning of data processing is",
+    answer: "Preparing information for business use",
+    options: [
+      "Doing a calculation",
+      "Collection of data",
+      "Preparing information for business use",
+      "None of these"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
